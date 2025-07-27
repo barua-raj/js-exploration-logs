0a9c6f8c56368c6ec9612155d11060bc4f2ae3c6
barua-raj <barua.jovraj@gmail.com>
2025-07-27 12:41:35 +0600

    use of js numbers, parsing and toFixed

---
+++ 01-intro-to-js/01-04-js-numbers.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 20;
+let c = parseInt('20');
+let d = parseFloat('25.50');
+
+var total = a + b + c + d;
+
+console.log(a + b + c);
+console.log(total);
+
+let numberA = 0.54;
+let numberB = 0.157;
+let totalNumber = parseFloat(numberA * numberB);
+
+console.log(totalNumber.toFixed(2));
