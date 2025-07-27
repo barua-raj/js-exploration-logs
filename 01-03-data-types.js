cd37e1ef4ccb23fa2d6450c621a7d69391635748
barua-raj <barua.jovraj@gmail.com>
2025-07-27 11:19:56 +0600

    data types and varaible naming in js

--- 01-03-data-types.js
+++ 01-03-data-types.js
@@ -1,2 +1,26 @@
+// integer
 let age = 50;
 console.log(age);
+console.log(typeof age);
+
+// string
+const country = "Bangladesh";
+console.log(country);
+console.log(typeof country);
+
+// boolean
+var pass = true;
+console.log(pass);
+console.log(typeof pass);
+
+// rules for naming variables
+    // 1. no keyword
+    // 2. no spcae in variable name
+    // 3. no quote
+    // 4. cannot start with a number or symbol
+    // 5. case sensitive
+    // 6. naming a long variable name (camel case)
+
+    let myCurrentHomeAddress = "Dhaka";
+    console.log(myCurrentHomeAddress);
+    console.log(typeof myCurrentHomeAddress);
