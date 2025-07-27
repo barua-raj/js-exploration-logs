9c9a40c5a2e32854b73e0b3ffd48bcbacceb4e4a
barua-raj <barua.jovraj@gmail.com>
2025-07-27 10:56:09 +0600

    declaring variables in js

--- 01-intro-to-js/01-02-declaring-variables.js
+++ 01-intro-to-js/01-02-declaring-variables.js
@@ -1,3 +1,4 @@
+// var is used in old versions of js
 var price = 550;
 var age = 50;
 var price = 600;
@@ -5,3 +6,8 @@ money = 700;
 console.log(age);
 console.log(price);
 console.log(money);
+
+// let or const is used in modern js
+
+let digit = 15;
+console.log(digit);
