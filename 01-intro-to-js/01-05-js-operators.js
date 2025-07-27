5a53e62259afc949496b3c9a86d7379371b53d1a
barua-raj <barua.jovraj@gmail.com>
2025-07-27 14:51:18 +0600

    js operators

---
+++ 01-intro-to-js/01-05-js-operators.js
@@ -0,0 +1,42 @@
+// addition
+let a = 10;
+let b = 20;
+let total = a + b;
+
+console.log(total);
+
+// subtraction
+let c = 10;
+let d = 20;
+let sub = c - d;
+
+console.log(sub);
+
+// multiplication
+let e = 10;
+let f = 20;
+let multipli = e * f;
+
+console.log(multipli);
+
+// division
+let g = 10;
+let h = 20;
+let div = g / h;
+
+console.log(div);
+
+// modulus
+let i = 5;
+let j = 2;
+let mod = i % j;
+
+console.log(mod);
+
+// shortend
+let k = 50;
+let l = 40;
+let shortadd = k + l;
+shortadd *= 10;
+
+console.log(shortadd);
