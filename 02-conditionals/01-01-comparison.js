c75694b883ab266825bbc674ada9212cb74be4b4
barua-raj <barua.jovraj@gmail.com>
2025-07-28 09:45:47 +0600

    conditional operators in JS

---
+++ 02-conditionals/01-01-comparison.js
@@ -0,0 +1,15 @@
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 10);
+console.log(10 >= 10);
+console.log(5 <= 10);
+
+let mangoPrice = 500;
+let applePrice = 600;
+
+console.log(mangoPrice > applePrice);
+
+console.log(10 == '10');
+console.log(10 === '10');
+console.log(10 != 10);
+console.log(10 !== '10');
