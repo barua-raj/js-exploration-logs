d12c0e7f0799e52a0796e059254c79e352e867e5
barua-raj <barua.jovraj@gmail.com>
2025-07-28 23:04:36 +0600

    completed practice task 01 : Free Drinks

---
+++ 02-conditionals/02-09-pt-01-free-drinks.js
@@ -0,0 +1,13 @@
+// Practice Task 01 : Free Drinks
+    // - Burger more than 500tk: free Coke
+    // - Else Coke: 30tk
+
+let burgerPrice = 700;
+let cokePrice = 30;
+
+if (burgerPrice > 500) {
+    console.log("You will get a free coke if buy a burger!");
+}
+else {
+    console.log("Single coke price would be", cokePrice,"taka!");
+}
