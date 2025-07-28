d6ce9f36b35b8bc7d86f747202ed438effe15361
barua-raj <barua.jovraj@gmail.com>
2025-07-28 12:23:19 +0600

    finding grade program using conditional

---
+++ 02-conditionals/02-03-grade-using-conditional.js
@@ -0,0 +1,22 @@
+let number = 69;
+if (number >= 80) {
+    console.log("You have got A+");
+}
+else if (number >= 70) {
+    console.log("You have got A");
+}
+else if (number >= 60) {
+    console.log("You have got A-");
+}
+else if (number >= 50) {
+    console.log("You have got B");
+}
+else if (number >= 40) {
+    console.log("You have got C");
+}
+else if (number >= 33) {
+    console.log("You have got D");
+}
+else {
+    console.log("You are failed!");
+}
