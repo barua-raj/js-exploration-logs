eb848e6cd1f7a9ba4b61299d655eefb2731ee75c
barua-raj <barua.jovraj@gmail.com>
2025-07-28 14:33:34 +0600

    finding even odd number using nested if

---
+++ 02-conditionals/02-03-even-odd-using-nested-if.js
@@ -0,0 +1,15 @@
+let num = 0;
+if (num > 0) {
+    if (num % 2 === 0) {
+        console.log("Even Number");
+    }
+    else {
+        console.log("Odd Number");
+    }
+}
+else if (num === 0) {
+    console.log("Zero");
+}
+else {
+    console.log("Negative Number");
+}
