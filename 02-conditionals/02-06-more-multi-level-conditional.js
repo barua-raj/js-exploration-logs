b47d9cc9c2eafe6548199c179d5d969f953ef5b9
barua-raj <barua.jovraj@gmail.com>
2025-07-28 15:58:13 +0600

    more multi level conditional practice

---
+++ 02-conditionals/02-06-more-multi-level-conditional.js
@@ -0,0 +1,18 @@
+const money = 700;
+
+if (money > 1000) {
+    console.log("Mutton");
+}
+else {
+    if (money > 900) {
+        console.log("Beef");
+    }
+    else {
+        if (money > 500) {
+            console.log("Chicken");
+         }
+         else {
+            console.log("Fish");
+         }
+    }
+}
