b9374d5535fa65d2ecbabf4c4374a9afde26b3b3
barua-raj <barua.jovraj@gmail.com>
2025-07-29 16:31:14 +0600

    practice task 06 - bus fare calculator

---
+++ 02-conditionals/02-12-pt-06-bus-fare-calculator.js
@@ -0,0 +1,25 @@
+// Practice Task 05 : Ticket Fare Calculator
+// Ticket fare Calculator
+//     - Children (age < 10): free
+//     - Students get a 50% discount
+//     - Senior citizens (age >= 60) gets a 15% Discount
+//     - Otherwise Regular ticket fare 800 tk
+
+const regularPrice = 800;
+const age = 25;
+const isStudent = true;
+
+if (age < 10) {
+  console.log("Free Ticket as you are a child!");
+}
+else if (isStudent === false) {
+    console.log("As you are a student, you will pay regular ticket fare which is",regularPrice,"Taka!");
+}
+else if (age >= 60) {
+  const discountPrice = regularPrice * (15 / 100);
+  const payAmount = regularPrice - discountPrice;
+  console.log("As you are a senior citizen, you will pay",payAmount,"Taka!");
+}
+else {
+    console.log("Regular ticker fare is",regularPrice,"Taka!");
+}
