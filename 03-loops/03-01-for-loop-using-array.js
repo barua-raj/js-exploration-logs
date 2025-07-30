f27aabc81f405ee7bfe8c7fb3689f64e3855c175
barua-raj <barua.jovraj@gmail.com>
2025-07-30 11:37:22 +0600

    for loop using array

---
+++ 03-loops/03-01-for-loop-using-array.js
@@ -0,0 +1,9 @@
+const numbers = [20, 12, 54, 58, 564, 214, 5];
+for (const num of numbers) {
+    console.log(num);
+}
+
+const fruits = ['apple', 'orange', 'watermelon', 'banana', 'mango']
+for (const fruit of fruits) {
+    console.log(fruit);
+}
