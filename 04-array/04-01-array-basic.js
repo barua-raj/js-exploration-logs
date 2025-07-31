461e21d2a3b19c615d5b1cb13784acaba6ffa6d5
barua-raj <barua.jovraj@gmail.com>
2025-07-31 13:04:57 +0600

    array basics

---
+++ 04-array/04-01-array-basic.js
@@ -0,0 +1,13 @@
+let fruits = ['apple', 'banana', 'mango', 'orange'];
+fruits[2] = 'watermelon';
+console.log(fruits[2]);
+
+// get element value by index
+let numbers = [20, 25, 65, 15, 12, 253, 951, 12, 41];
+console.log(numbers[0]);
+
+
+// set or update value
+
+numbers[5] = 5550;
+console.log(numbers[5]);
