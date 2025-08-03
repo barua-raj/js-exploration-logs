5d983a44238b3dc177c1b70fd42dd95a38317478
barua-raj <barua.jovraj@gmail.com>
2025-08-03 12:08:22 +0600

    do while loop

---
+++ 03-loops/03-08-do-while-loop.js
@@ -0,0 +1,14 @@
+let i = 0;
+while (i <= 5) {
+    console.log(i);
+    i++
+}
+
+let j = 10;
+do {
+    console.log(j);
+    j++;
+}
+while (j < 5) {
+
+}
---
+++ 04-array/04-03-pop.js
@@ -0,0 +1,11 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam'];
+friends.push('rahim');
+console.log(friends);
+
+const out1 = friends.pop();
+console.log(out1);
+
+const out2 = friends.pop();
+console.log(out2);
+
+console.log(out1, out2);
