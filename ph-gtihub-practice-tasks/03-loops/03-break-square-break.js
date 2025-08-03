142cf24753f752b777d70e6640d10ad551b5c6ac
barua-raj <barua.jovraj@gmail.com>
2025-08-03 13:47:39 +0600

    have not solved yet! need to work later!

---
+++ ph-gtihub-practice-tasks/03-loops/03-break-square-break.js
@@ -0,0 +1,10 @@
+// Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+
+let i = 1;
+let n = 1;
+
+while(i >= 100) {
+    console.log(i);
+    n = i * i;
+    if (n === )
+}
