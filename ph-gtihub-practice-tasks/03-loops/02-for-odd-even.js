09e78e0663e31785e426f6269239a0eeffceb644
barua-raj <barua.jovraj@gmail.com>
2025-08-03 12:47:59 +0600

    odd even numbers using for loop

---
+++ ph-gtihub-practice-tasks/03-loops/02-for-odd-even.js
@@ -0,0 +1,22 @@
+// Subtask-1:
+// Find all the odd numbers from 61 to 100.
+
+// Subtask-2:
+
+// Find all the even numbers from 78 to 98.
+
+console.log("All the odd numbers from 61 to 100:");
+for (let i = 61; i <= 100; i++) {
+    if (i % 2 === 1) {
+        console.log(i);
+    }
+}
+
+console.log("\n");
+
+console.log("All the even numbers from 78 to 98:");
+for (let j = 78; j <= 98; j++) {
+    if (j % 2 === 0) {
+        console.log(j);
+    }
+}
