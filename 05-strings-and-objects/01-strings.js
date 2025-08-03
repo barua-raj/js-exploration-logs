122ba0b82c8ecf43c8a8d424e31ea9815eb7f1f9
barua-raj <barua.jovraj@gmail.com>
2025-08-03 15:24:55 +0600

    strings basics

---
+++ 05-strings-and-objects/01-strings.js
@@ -0,0 +1,15 @@
+const country = 'Bangladesh';
+const city = "Dhaka";
+const policeStation = `Gulshan`;
+const areaName = new String('Niketon');
+
+console.log(typeof areaName);
+
+const numbers = [55, 15, 85, 12, 65];
+
+console.log(numbers.length);
+console.log(numbers[4]);
+
+console.log(city.length);
+console.log(city[3]);
+
