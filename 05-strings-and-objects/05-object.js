8bee6689c8f91fd7db67e58cd8563adc7fbe3208
barua-raj <barua.jovraj@gmail.com>
2025-08-03 17:19:40 +0600

    object in JavaScript

---
+++ 05-strings-and-objects/05-object.js
@@ -0,0 +1,19 @@
+const bottle = {
+    brand: 'kinley',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+console.log(bottle);
+
+const subject = {
+    name: 'biology',
+    teacher: 'Rahim Uddin',
+    chapters: ['first', 'second', 'third', 'fourth'],
+    examInfo: {
+        examName: 'Final',
+        marks: 100,
+        examDate: '15 September'
+    }
+}
+console.log(subject);
