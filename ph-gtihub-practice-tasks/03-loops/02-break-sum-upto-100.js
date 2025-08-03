b5480b5bac48dd9c5955305230507775bc6feecf
barua-raj <barua.jovraj@gmail.com>
2025-08-03 13:34:41 +0600

    sum upto 100 using break

---
+++ ph-gtihub-practice-tasks/03-loops/02-break-sum-upto-100.js
@@ -0,0 +1,13 @@
+// Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+
+let i = 1;
+let sum = 0;
+while (i <= 20) {
+    if (sum + i >= 100) {
+        break;
+    }
+    sum = sum + i;  
+    i++;
+}
+console.log(sum);
+console.log(i);
