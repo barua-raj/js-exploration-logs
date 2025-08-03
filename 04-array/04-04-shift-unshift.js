477df6007d60f1259bcc22eecdd0063c1e31f345
barua-raj <barua.jovraj@gmail.com>
2025-08-03 15:04:04 +0600

    shift and unshift in array

---
+++ 04-array/04-04-shift-unshift.js
@@ -0,0 +1,8 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam', 'roilam'];
+console.log(friends);
+
+// friends.shift();
+// console.log(friends);
+
+friends.unshift('abul');
+console.log(friends);
