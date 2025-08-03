7c21e900ec9fa00cc5c6827bf1abdcaa6325556f
barua-raj <barua.jovraj@gmail.com>
2025-08-03 12:34:20 +0600

    multiplication table of 5

---
+++ ph-gtihub-practice-tasks/03-loops/04-while-multiplication-of-5.js
@@ -0,0 +1,12 @@
+// As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+// 
+
+
+let i = 1;
+let number = 5;
+console.log("Multiplication table of 5:");
+while (i <= 10) {
+    let mul = number * i;
+    console.log("5 * ",i,"=",mul);
+    i++;
+}
