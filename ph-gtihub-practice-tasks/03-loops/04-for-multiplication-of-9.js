2db864d3d3af975826e8741f176297867297e326
barua-raj <barua.jovraj@gmail.com>
2025-08-03 13:13:42 +0600

    multiplication table of  number of 9

---
+++ ph-gtihub-practice-tasks/03-loops/04-for-multiplication-of-9.js
@@ -0,0 +1,8 @@
+// Generate a multiplication table for number 9
+
+let number = 9;
+console.log("Multiplication table for number 9:");
+for (let i = 1; i <= 9; i++) {
+    mul = number * i;
+    console.log(mul);
+}
