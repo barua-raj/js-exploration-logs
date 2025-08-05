df27d6eb6f9989ec6b394b35d9ea841de0d2e266
barua-raj <barua.jovraj@gmail.com>
2025-08-05 15:31:31 +0600

    function basics

---
+++ 06-function/01-basic-function.js
@@ -0,0 +1,19 @@
+function firstFunction() {
+    console.log("This is a basic fumction in JS!");
+}
+firstFunction();
+
+function moneyBag() {
+    var money = 5000;
+    console.log("I have",money,"in my moneybag!");
+    console.log("I have " + money + " in my moneybag!");
+}
+moneyBag();
+
+function sumOfTwo(input1, input2) {
+    let sum = input1 + input2;
+    console.log(sum);
+    return sum;
+}
+const result = sumOfTwo(20, 30);
+console.log(result);
