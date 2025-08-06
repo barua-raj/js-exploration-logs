bad10fb8a82fc8874b777eb4a8e8db5eee226772
barua-raj <barua.jovraj@gmail.com>
2025-08-07 00:42:46 +0600

    revision

---
+++ 07-array-traversal-operation/revision.js
@@ -0,0 +1,51 @@
+const price = 500;
+console.log(price);
+
+console.log("\n");
+
+const ceName = "F. R. Mahmood Hasan"
+console.log(ceName);
+
+console.log("\n");
+
+const isRich = true;
+console.log(isRich);
+
+console.log("\n");
+
+const friends = ['Anik', 'Zayed', 'Rajib', 'Sajib'];
+console.log(friends);
+console.log(friends[1]);
+
+console.log("\n");
+
+const studentInfo = {name: 'tihan', class: 9, age: 17, address: 'Dhaka'}
+console.log(studentInfo);
+
+console.log("\n");
+
+const amount = 2200;
+if (amount > 1000) {
+    console.log("Buy");
+}
+else if (amount > 500) {
+    console.log("Buy");
+}
+else {
+    console.log("Do not buy");
+}
+
+console.log("\n");
+
+let i = 0;
+while (i <= 5) {
+    console.log("Count - ", i);
+    i++;
+}
+
+console.log("\n");
+
+for (let j = 5; j >= 0; j--) {
+    console.log("Count - ", j);
+    
+}
