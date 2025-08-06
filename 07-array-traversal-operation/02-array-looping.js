23c8974a94678345e9902be6fa88db94429ad89d
barua-raj <barua.jovraj@gmail.com>
2025-08-07 01:01:41 +0600

    array looping

---
+++ 07-array-traversal-operation/02-array-looping.js
@@ -0,0 +1,26 @@
+const fruits = ['Apple', 'Banana', 'Anar', 'Watermelon', 'Coconut'];
+// console.log(fruits);
+
+for (let i = 0; i < fruits.length; i++) {
+    // console.log(i);
+    // console.log(fruits[i]);
+}
+
+const numbers = [12, 43, 56, 76, 45, 41, 90];
+for (let j = 0; j < numbers.length; j++){
+    // console.log(numbers[j]);
+}
+
+const age = [43, 23, 65, 12, 90, 80, 67];
+let k = 0;
+while (k < age.length) {
+    // console.log(age[k]);
+    k++;
+}
+
+const price = [34, 76, 12, 65, 23, 44, 57, 89]
+let n = 0;
+while (n < price.length) {
+    console.log(price);
+    n++;
+}
