755a205b9c44aa2e86c07650ce1523a5d3b31f89
barua-raj <barua.jovraj@gmail.com>
2025-08-09 01:48:43 +0600

    problem 01 solved

---
+++ ph-gtihub-practice-tasks/08-functions/01-multiply-four.js
@@ -0,0 +1,8 @@
+// Take four parameters. Multiply the four numbers and then return the result.
+
+function multiplyOfFour(num1, num2, num3, num4){
+    const result = num1 * num2 * num3 * num4;
+    return result;
+}
+const resultMultiply = multiplyOfFour(5, 2, 3, 4);
+console.log(resultMultiply);
