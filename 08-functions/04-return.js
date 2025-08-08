5ec5159036288577196361882e905a0dd29b4860
barua-raj <barua.jovraj@gmail.com>
2025-08-08 13:12:59 +0600

    return

---
+++ 08-functions/04-return.js
@@ -0,0 +1,9 @@
+function multiThree(a, b, c){
+    const result = a * b * c;
+    return result;
+}
+const finalResult = multiThree(3, 5, 2);
+const finalResult2 = finalResult + 10;
+console.log(finalResult2);
+
+
