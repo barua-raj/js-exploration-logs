03379d98b5835f2ce53ea295aa5ff416ec74feae
barua-raj <barua.jovraj@gmail.com>
2025-08-08 12:42:25 +0600

    parameters of function

---
+++ 08-functions/03-parameter.js
@@ -0,0 +1,20 @@
+function square(number){
+    const result = number * number;
+    console.log(result);
+}
+square(4);
+square(12);
+square(593);
+
+function sum(a, b) {
+    const sumResult = a + b;
+    console.log(sumResult);
+}
+sum(4, 6);
+sum(10, 547);
+
+function sumAll(a, b, c, d, e){
+    const sumOfAll = a+ b + c + d + e;
+    console.log(sumOfAll);
+}
+sumAll(10, 20, 30, 40, 50);
