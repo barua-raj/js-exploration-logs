5f9cdf5a463c54df6ede723508b9b4deadf29ddb
barua-raj <barua.jovraj@gmail.com>
2025-08-10 17:30:16 +0600

    usage of some math function

---
+++ 09-js-simple-coding-problems-part-01/05-some-math-function.js
@@ -0,0 +1,22 @@
+const minNumber = Math.min(34, 123, 54, 12, 4, 90, 3, -3, -59, 54, 23, 12, -1);
+console.log(minNumber);
+
+const maxNumber = Math.max(34, 123, 54, 12, 4, 90, 3, -3, -59, 54, 23, 12, -1);
+console.log(maxNumber);
+
+console.log(Math.PI.toFixed(2));
+
+console.log(Math.abs(10-88));
+
+console.log(Math.round(5.50));
+
+console.log(Math.floor(1453.99));
+
+console.log(Math.ceil(120.201));
+
+console.log(Math.random());
+
+console.log(Math.random()*20);
+
+const ran = Math.round(Math.random()*20);
+console.log(ran);
