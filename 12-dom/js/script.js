f68023e0bc450280d14c1d99fef522cea2842047
barua-raj <barua.jovraj@gmail.com>
2025-08-19 14:42:09 +0600

    DOM

---
+++ 12-dom/js/script.js
@@ -0,0 +1,32 @@
+console.log("Hello from external link");
+const student = {
+    name: 'Abul Mia',
+    id: 150,
+    study: function (subject){
+        // console.log('studying:', subject);
+    }
+}
+// document.getElementsByTagName
+const liCollection = document.getElementsByTagName('li');
+console.log(liCollection);
+for (const li of liCollection){
+    // console.log(li.innerText);
+}
+const allHeadings = document.getElementsByTagName('h1');
+for (const heading of allHeadings){
+    // console.log(heading.innerText);
+}
+const foodTitleH1 = document.getElementById('food-title');
+console.log(foodTitleH1.innerText);
+foodTitleH1.innerText = "Foods I hate to eat";
+
+const nameItem = document.getElementById('name');
+nameItem.innerText = "Wrong Name";
+
+const foreignElements = document.getElementsByClassName('foreign-food');
+foreignElements.innerText = 'No';
+console.log(foreignElements);
+
+for (const element of foreignElements){
+    console.log(element);
+}
