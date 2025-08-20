a1ca3a4c0556829a1fd4a72207a749ed4c9fbe3d
barua-raj <barua.jovraj@gmail.com>
2025-08-20 17:04:42 +0600

    dom get/set attribute

--- 12-dom/js/script.js
+++ 12-dom/js/script.js
@@ -6,6 +6,8 @@ const student = {
         // console.log('studying:', subject);
     }
 }
+
+
 // document.getElementsByTagName
 const liCollection = document.getElementsByTagName('li');
 console.log(liCollection);
@@ -16,6 +18,9 @@ const allHeadings = document.getElementsByTagName('h1');
 for (const heading of allHeadings){
     // console.log(heading.innerText);
 }
+
+
+// document.getElementById
 const foodTitleH1 = document.getElementById('food-title');
 console.log(foodTitleH1.innerText);
 foodTitleH1.innerText = "Foods I hate to eat";
@@ -23,10 +28,19 @@ foodTitleH1.innerText = "Foods I hate to eat";
 const nameItem = document.getElementById('name');
 nameItem.innerText = "Wrong Name";
 
+
+// document.getElementByClassName
 const foreignElements = document.getElementsByClassName('foreign-food');
 foreignElements.innerText = 'No';
 console.log(foreignElements);
 
 for (const element of foreignElements){
     console.log(element);
+}
+
+
+// document.querySelectorAll
+const spanElements = document.querySelectorAll('#old-dhaka p span')
+for (const span of spanElements) {
+    console.log(span.innerText);
 }
