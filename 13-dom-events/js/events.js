823b4677825ad7a8809c777729f9bed6b3d34dd9
barua-raj <barua.jovraj@gmail.com>
2025-08-23 03:03:18 +0600

    onclick events

---
+++ 13-dom-events/js/events.js
@@ -0,0 +1,24 @@
+// option 2 to handle events
+
+function makeYellow(){
+    document.body.style.background = 'yellow';
+}
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+// option 3 to handle events: get elements by ID and set onclick
+
+const btnMakeBlue = document.getElementById('btn-make-blue');
+// console.log(btnMakeBlue);
+btnMakeBlue.onclick = function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+// option 3: slight different
+
+const btnMakePurple = document.getElementById('btn-make-purple');
+btnMakePurple.onclick = makePurple;
+function makePurple(){
+    document.body.style.backgroundColor = 'purple';
+}
