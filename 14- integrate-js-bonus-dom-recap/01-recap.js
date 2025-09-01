d53c604e5c2a7d6e646e051511c5f09d1c771ea1
barua-raj <barua.jovraj@gmail.com>
2025-09-01 15:02:47 +0600

    bonus- dom recap & validation

---
+++ 14- integrate-js-bonus-dom-recap/01-recap.js
@@ -0,0 +1,17 @@
+// 1. how to use js with html and css
+// 2. get element from dom
+//     1. getElementByID
+//     2. getElementsByClassName
+//     3. getElementByTagName
+//     4. querySelector: return first element from the matched element
+//     5. querySelectorAll
+// 3. dynamically add element to the dom: document.createElement
+//     1. set value to the innerHTML
+// 4. get and set value from/to a dom element
+//     1. use innerText (h1, p, div……….)
+//     2. use value (input, textArea)
+// 5. add event listener (event handler)
+//     1. click, focus, blur, onchange, keyup
+//     2. onclick = “myhandler()”
+// 6. use function to reduce duplicate code
+// 7. event bubble and delegate
