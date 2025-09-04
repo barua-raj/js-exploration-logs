6c711d57acc335fb49a9d4a14fa7ade7cdf437fd
barua-raj <barua.jovraj@gmail.com>
2025-09-04 10:11:00 +0600

    entries/freeze/seal/object-looping

---
+++ 15-es6-intro/07-freeze.js
@@ -0,0 +1,12 @@
+const employee = {
+    name: 'abul',
+    designation: 'designer',
+    salary: 20000,
+    experience: 1,
+    age: 22
+}
+// delete employee.experience;
+Object.freeze(employee);
+employee.salary = employee.salary + 5000;
+employee.location = 'Dhaka';
+console.log(employee);
