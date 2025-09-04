7e81b5e640778802c74618605e4ce27330350b1c
barua-raj <barua.jovraj@gmail.com>
2025-09-04 16:58:34 +0600

    filter wip

---
+++ 16-more-es6/05-filter.js
@@ -0,0 +1,9 @@
+const numbers = [1, 10, 5, 40, 50];
+const result = numbers.filter(num => num > 10);
+console.log(result);
+const evenNumber = numbers.filter(num => {
+    if (num % 2 === 0) {
+        console.log();
+        
+    }
+})
