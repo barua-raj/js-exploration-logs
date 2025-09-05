c60e9e10d6c9e921d3bf7e6515514e7ce9c5f7e8
barua-raj <barua.jovraj@gmail.com>
2025-09-05 13:18:49 +0600

    filter and find

--- 16-more-es6/05-filter.js
+++ 16-more-es6/05-filter.js
@@ -1,9 +1,16 @@
+// const numbers = [1, 10, 5, 40, 50];
+// const result = numbers.filter(num => num > 10);
+// console.log(result);
+// const evenNumber = numbers.filter(num => {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//     }
+// })
+
 const numbers = [1, 10, 5, 40, 50];
-const result = numbers.filter(num => num > 10);
-console.log(result);
-const evenNumber = numbers.filter(num => {
+const evenNumber = numbers.find(num => {
     if (num % 2 === 0) {
-        console.log();
-        
+        console.log(num);
+        return num;
     }
 })
