5551429df0c61be5d518a5f7cab49e45a3b2ee75
barua-raj <barua.jovraj@gmail.com>
2025-09-07 14:54:31 +0600

    to-do api call and display

---
+++ 18-api-json-data-load-dynamic-website/to-do.js
@@ -0,0 +1,24 @@
+const loadToDo = () => {
+    const url = "https://jsonplaceholder.typicode.com/todos";
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayToDo(data));
+};
+const displayToDo = (todos) => {
+    const todoContainer = document.getElementById("to-do-container");
+    todoContainer.innerHTML = "";
+    todos.forEach((todo) => {
+        console.log(todo);
+        const todoCard = document.createElement("div");
+        todoCard.innerHTML = `
+            <div class="todo-card">
+                <p>${todo.completed == true
+                    ? `<i class="fa-solid fa-square-check"></i>`
+                    : `<i class="fa-regular fa-square-check"></i>` }</p>
+                <h4>${todo.title}</h4>
+            </div>
+        `;
+        todoContainer.append(todoCard)
+    });
+}
+loadToDo();
